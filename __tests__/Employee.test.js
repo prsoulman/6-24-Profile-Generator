6922cbf323ecd28128378e194cdb16fe06951b49
David Rodriguez <davidrodriguezcomedy@gmail.com>
2022-06-24 09:56:24 -0500

    added new pages

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,48 +1,47 @@
 const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
-  const e = new Employee();
+  const e = new Employee(1, 'john', 'test@test.com');
   expect(typeof(e)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
-  const name = "Alice";
-  const e = new Employee(name);
-  expect(e.name).toBe(name);
+  const e = new Employee(1, 'john', 'test@test.com');
+  expect(e.name).toBe('john');
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
-  expect(e.id).toBe(testValue);
+  const e = new Employee(1, 'john', 'test@test.com');
+  expect(e.id).toBe(1);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
-  expect(e.email).toBe(testValue);
+  const e = new Employee(1, 'john', 'test@test.com');
+  expect(e.email).toBe('test@test.com');
 });
 
 test("Can get name via getName()", () => {
   const testValue = "Alice";
-  const e = new Employee(testValue);
-  expect(e.getName()).toBe(testValue);
+  const e = new Employee(1, 'john', 'test@test.com');
+  expect(e.getName()).toBe('john');
 });
 
 test("Can get id via getId()", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
-  expect(e.getId()).toBe(testValue);
+  const e = new Employee(1, 'john', 'test@test.com');
+  expect(e.getId()).toBe(1);
 });
 
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
-  expect(e.getEmail()).toBe(testValue);
+  const e = new Employee(1, 'john', 'test@test.com');
+  expect(e.getEmail()).toBe('test@test.com');
 });
 
 test("getRole() should return \"Employee\"", () => {
-  const testValue = "Employee";
+  const testValue = "employee";
   const e = new Employee("Alice", 1, "test@test.com");
   expect(e.getRole()).toBe(testValue);
 });
--- index.js
+++ index.js
@@ -1,3 +1,44 @@
 import inquirer from 'inquirer';
 import fs from 'fs'; 
+import Manager from './lib/Manager.js'
 
+const mainMenu = () => {
+    inquirer.prompt({
+        type: 'list',
+        name: 'Direction',
+        message: 'What type of employee would you like to create',
+        choices: ['Manager', 'Engineer', 'Intern', 'Build Team'],
+    }) 
+    .then (({Direction}) => {
+        console.log(Direction);
+        if (Direction==='Manager') {
+            askManager()
+        }
+    }) 
+}
+const askManager = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'id',
+        message: 'What is the employees ID?',
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'What is the employees name?',
+    },
+    {
+        type: 'input',
+        name: 'email', 
+        message: 'What is the employees email?',
+    },
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: 'What is the managers office number?',
+    }
+]) .then(({id, name, email, officeNum}) => {
+    const manager = new Manager(id, name, email ,officeNum)
+    console.log(manager);
+})
+} 
+mainMenu()
