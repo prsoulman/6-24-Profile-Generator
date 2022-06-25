710c24927dc23f5032acdbf9ecb757f456b71741
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-06-25 12:10:29 -0500

    added other employees with conditional menu

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import inquirer from 'inquirer';
-import fs from 'fs'; 
+//import fs from 'fs'; 
 import Manager from './lib/Manager.js'
+import Engineer from './lib/Engineer.js';
+import Intern from './lib/Intern.js';
+import Employee from './lib/employee.js';
 
 const mainMenu = () => {
     inquirer.prompt({
@@ -13,8 +16,15 @@ const mainMenu = () => {
         console.log(Direction);
         if (Direction==='Manager') {
             askManager()
+        } else if (Direction==='Engineer') {
+            askEngineer()
+        } else if (Direction==='Intern') {
+            askIntern()
+        }else {
+            askEmployee()
         }
-    }) 
+        
+    }); 
 }
 const askManager = () => {
     inquirer.prompt([{
@@ -39,6 +49,91 @@ const askManager = () => {
 ]) .then(({id, name, email, officeNum}) => {
     const manager = new Manager(id, name, email ,officeNum)
     console.log(manager);
+    mainMenu()
+})
+
+} 
+
+
+const askEngineer = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'id',
+        message: 'What is the employees ID?',
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'What is the employees name?',
+    },
+    {
+        type: 'input',
+        name: 'email', 
+        message: 'What is the employees email?',
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your GitHub URL?',
+    }
+]) .then(({id, name, email, gitHub}) => {
+    const engineer = new Engineer (id, name, email ,gitHub)
+    console.log(engineer);
+})
+} 
+//mainMenu()
+
+const askIntern = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'id',
+        message: 'What is the employees ID?',
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'What is the employees name?',
+    },
+    {
+        type: 'input',
+        name: 'email', 
+        message: 'What is the employees email?',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is the name of your school?',
+    }
+]) .then(({id, name, email, school}) => {
+    const intern = new Intern (id, name, email ,school)
+    console.log(intern);
+})
+} 
+//mainMenu()
+
+const askEmployee = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'id',
+        message: 'What is the employees ID?',
+    }, {
+        type: 'input',
+        name: 'name',
+        message: 'What is the employees name?',
+    },
+    {
+        type: 'input',
+        name: 'email', 
+        message: 'What is the employees email?',
+    },
+    {
+        // type: 'input',
+        // name: 'officeNum',
+        // message: 'What is the managers office number?',
+    }
+]) .then(({id, name, email}) => {
+    const employee = new Employee(id, name, email)
+    console.log(employee);
 })
 } 
 mainMenu()
+
+
