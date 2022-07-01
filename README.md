5d4c49575d8a79f2cb8e514efa4b8180b2285e14
prsoulman <60936175+prsoulman@users.noreply.github.com>
2022-06-30 19:18:38 -0500

    tests work now

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 
 
-#Description
-This is an application that makes a profile team.
+**Description**
+This is an application that makes a profile team using node.js and jest and inquirer.
 
-#Technologies Used
+**Technologies Used**
 -*Javascript*
 -*node.js*
+-*inquirer*
+-*Jest*
+
+
+![Tests Passing](./Untitled_%20Jun%2030%2C%202022%207_13%20PM.gif)
 
 
 Walkthrough Video 
---
+++ test/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("./lib/");
+
+test("Can set GitHUb account via constructor", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer(1, 'john', 'test@test.com', testValue);
+  expect(e.gitHub).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+  const testValue = "Engineer";
+  const e = new Engineer(1, 'john', 'test@test.com', "GitHubUser");
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username via getGithub()", () => {
+  const testValue = "GitHubUser";
+  const e = new Engineer(1, 'john', 'test@test.com', testValue);
+  expect(e.getGitHub()).toBe(testValue);
+});
